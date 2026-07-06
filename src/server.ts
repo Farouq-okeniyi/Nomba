4138ade6dd2d8d61979f0c4bdd31e7f284dbb042
bogo <farouqokeniyi2004@gmail.com>
2026-07-06 03:00:07 +0100

    (fix: ) bad cors reflight request

--- src/server.ts
+++ src/server.ts
@@ -29,8 +29,8 @@ app.use(express.json({
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'nomba-signature', 'x-api-key'],
   credentials: false,
 }));
 app.use(morgan('combined'));
